a956ec43cc52d374c693a1faded6841f21836b40
Frodo <83783418+frodobigens@users.noreply.github.com>
2024-05-16 23:34:44 +0300

    Update axelar.sh

--- axelar.sh
+++ axelar.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-RPC_URL="https://tm.axelar.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://tm.axelar.lava.build/lava-referer-314e8c9d-bb29-4f86-9176-a6242ebeaa23/"
 
 OUTPUT_DIR="logs"
 
-axelar_data=$(curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","id":"1","method":"status"}' "$RPC_URL")
+UA=$(shuf -n 1 ua.txt)
+
+axelar_data=$(curl -A UA -x http://84.247.60.38:6008 --proxy-user cwmgurpo:3lv7ii9r7e6p -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","id":"1","method":"status"}' "$RPC_URL")
 
 if [ $? -eq 0 ]; then
     if [ ! -d "$OUTPUT_DIR" ]; then
